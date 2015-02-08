674b0501459223089f0e8d4aa0eb4ee8f9bb9642
nosedive25 <nosedive25@git.git>
2015-02-08 15:53:46 -0500

    Started Dashboard

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -1,9 +1,23 @@
 jasper.controller('dashboardController',['$scope', '$state', '$http', '$timeout', function($scope, $state, $http, $timeout) {
+	$scope.stats = {'working' : 0, 'repair' : 0};
+	$scope.repairs = [];
+
 	$http({
 	    method  : 'GET',
 	    url     : '/builds/list'
 	 })
 	.success(function(resp) {
-	    $scope.builds = angular.fromJson(resp);
+	    var builds = angular.fromJson(resp);
+
+	    for (var osType in builds) {
+	    	for (var i = 0; i < builds[osType].length; i++) {
+	    		if (builds[osType][i].imageStatus == "Working") {
+	    			$scope.stats.working++;
+	    		} else if (builds[osType][i].imageStatus != "New") {
+	    			$scope.stats.repair++;
+	    			$scope.repairs.push(builds[osType][i]);
+	    		}
+	    	}
+	    }
 	});
 }]);
